57f5d38bb336dddfda7bf23a0e63ae6b4d627666
Mijecu25 <main@mijecu25.com>
2014-09-18 20:06:30 -0500

    MAV reading a text file
    Wrote, tested, and commented the code to read a .txt file. This file has
    the commands that we will use for our project.

--- main.c
+++ main.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main (int *argc, char * argv[] ) {
-    printf("Hello team Awesome!!!\n");
+void readCommands(char commands[]) {
+    FILE * filePointer; // File pointer
+    filePointer = fopen(commands, "r"); //opens the filename pointed to by filename using the given mode.
+    char line[128]; // variable that holds each line that is read
 
+    // File EOF. Keep reading until the file pointer reaches the end of the file
+    while(!feof(filePointer)) {
+        // Gets a line from the file
+        fgets(line, 128, filePointer);
+
+        // Print in command line
+        fputs(line, stdout);
+    }
+
+    // Closing the file
+    fclose(filePointer);
+}
+
+
+int main (int *argc, char *argv[] ) {
+    static const char commands[] = "commands.txt"; // File containing the commands
+
+    readCommands(commands);
     return 0;
 };
 
