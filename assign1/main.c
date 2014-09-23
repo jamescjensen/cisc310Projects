473820f5693cf56e67b8f32efe2be43eb30be3b3
Miguel Velez <miguelvelez@mijecu25.com>
2014-09-23 08:11:34 -0500

    MAV working on testing
    Committed the current files to the testing branch.

--- assign1/main.c
+++ assign1/main.c
@@ -1,5 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define RUNNING 0
+#define READY 1
+#define WAITING 2
+#define FINISHED 4
+
+#define NEW 10
+#define SSD 11
+#define INP 12
+
+struct process {
+    int state;
+    int id;
+    int next_index;
+    int ssd_accesses;
+    int wait_time;
+    struct command *commands[150];
+};
+
+struct command {
+    int name;
+    int time;
+};
+
+/*
+struct ssd {
+    process *pcs;
+};
+
+struct cpu {
+    process *pcs;
+};
+
+struct cpu_q {
+    cpu *cpus;
+};
+
+struct ssd_q {
+    ssd *ssds;
+};
+*/
 
 void readCommands(char commands[]) {
     FILE * filePointer; // File pointer
@@ -11,19 +53,40 @@ void readCommands(char commands[]) {
         // Gets a line from the file
         fgets(line, 128, filePointer);
 
+        char * pch; // TODO what is this?
+        pch = strtok (line, " "); // TODO what is this doing?
+
+        while(pch != NULL) {
+            // TODO Be careful since it might be reading the new lines as a separate character
+            // TODO is this doing string comparison?
+            if (strcmp(pch,"NEW")==0) {
+                printf("NEW\n");
+            } else if(strcmp(pch,"CPU")== 0) {
+                printf("CPU\n");
+            } else if(strcmp(pch,"INP")== 0) {
+                printf("INP\n");
+            }
+
+            // TODO what is this doing?
+            pch = strtok (NULL, " ");
+
+            if(pch != NULL) {
+                printf ("le number is %s\n", pch);
+            }
+
+        }
         // Print in command line
-        fputs(line, stdout);
+        //fputs(line, stdout);
     }
 
     // Closing the file
     fclose(filePointer);
 }
 
-
-int main (int *argc, char *argv[] ) {
-    static const char commands[] = "input.txt"; // File containing the commands
-
-    readCommands(commands);
+int main (int argc, char *argv[]) {
+    static const char input[] = "input.txt"; // File containing the commands
+    //extern struct // TODO this was creating an compile error when not commented.
+    readCommands(input);
     return 0;
 };
 
