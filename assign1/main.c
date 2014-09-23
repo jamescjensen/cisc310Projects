f874ee2f1fdc71ed7f8919733eac18df3a081b99
Miguel Velez <miguelvelez@mijecu25.com>
2014-09-23 08:58:24 -0500

    MAV main logic
    Wrote a bunch of TODOs describing the main logic of the program.

--- assign1/main.c
+++ assign1/main.c
@@ -5,11 +5,14 @@
 #define RUNNING 0
 #define READY 1
 #define WAITING 2
-#define FINISHED 4
+#define FINISHED 3
 
 #define NEW 10
 #define SSD 11
 #define INP 12
+
+
+// TODO finish adding the variables needed for each struct.
 
 struct process {
     int id;
@@ -44,7 +47,7 @@ struct ssd_q {
 };
 */
 
-void readCommands(char commands[]) {
+void read_commands(char commands[]) {
     FILE * filePointer; // File pointer
     filePointer = fopen(commands, "r"); //opens the filename pointed to by filename using the given mode.
     char line[128]; // variable that holds each line that is read
@@ -62,11 +65,13 @@ void readCommands(char commands[]) {
             // TODO is this doing string comparison?
             if (strcmp(pch,"NEW")==0) {
                 printf("NEW\n");
-                // TODO create a new process struct
+                // TODO create a new process struct. Then add this command to the command array.
             } else if(strcmp(pch,"CPU")== 0) {
                 printf("CPU\n");
+                //TODO add this command to the command array of the current process
             } else if(strcmp(pch,"INP")== 0) {
                 printf("INP\n");
+                //TODO add this command to the command array of the current process
             }
 
             // TODO what is this doing?
@@ -86,22 +91,52 @@ void readCommands(char commands[]) {
 }
 
 // Method that creates and initializes a struct.
-void createStruct() { // TODO some information about the process should be passed to this function.
-    static int processId = 0;
+void create_struct() { // TODO some information about the process should be passed to this function.
+    static int process_id = 0;
 
-    printf("%d\n", processId);
+    // Create process
+    struct process pr;
 
+    // Initialize variables
+    pr.id = process_id;
+    pr.state = READY;
+    pr.next_index = 0;
+    pr.ssd_accesses = 0;
+    pr.wait_time_ssd = 0;
+    pr.time_ssd = 0;
+    // struct command *commands[150]; // TODO initialize this struct.
 
-    processId++;
+    // Put the process in the Process Table array
+
+    // Put process in ready queue
+    printf("%d\n", process_id);
+
+    process_id++;
+}
+
+// Method that takes a process and puts the commands for that process in its commands array.
+void read_write_commands() {
+    //TODO Be sure to include the NEW XXX as the first command.
+
 }
 
 int main (int argc, char *argv[]) {
     static const char input[] = "input.txt"; // File containing the commands
     //extern struct // TODO this was creating an compile error when not commented.
-    readCommands(input);
+
+    // TODO Initialize Process Table array.
+    // TODO Initialize CPU 1.
+    // TODO Initialize CPU 2.
+    // TODO Initialize SSD.
+    // TODO Initialize Ready queue.
+    // TODO Initialize SSD queue.
+    // TODO Initialize Waiting queue.
+    // TODO Initialize Finished Process array.
+
+    read_commands(input);
+
+    // TODO execute main logic.
+    //
+
     return 0;
 };
-
-
-
-
